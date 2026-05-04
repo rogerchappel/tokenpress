9c7acefba63e15bafc8b158fba59ee8d59ecffb2
Roger Chappel <miscanalysis@gmail.com>
2026-05-04 18:34:38 +1000

    feat: expose library api

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { extractCommand, extractExitCode, extractPaths, isDecisionLine, isErrorLine } from "./detect.js";
+export { pressTranscript } from "./press.js";
+export { renderJson, renderMarkdown } from "./render.js";
+export { redactLine } from "./redact.js";
+export type { AdapterName, CommandBlock, EvidenceLine, OutputFormat, PressedTranscript, PressOptions } from "./types.js";
