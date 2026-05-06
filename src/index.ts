5b1096d1f7c3640dd16bce93ab4e4a2325e09569
Roger Chappel <miscanalysis@gmail.com>
2026-05-07 06:36:58 +1000

    chore: export summary type

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ export { extractCommand, extractExitCode, extractPaths, isDecisionLine, isErrorL
 export { pressTranscript } from "./press.js";
 export { renderJson, renderMarkdown } from "./render.js";
 export { redactLine } from "./redact.js";
-export type { AdapterName, CommandBlock, EvidenceLine, OutputFormat, PressedTranscript, PressOptions } from "./types.js";
+export type { AdapterName, CommandBlock, EvidenceLine, OutputFormat, PressedTranscript, PressOptions, PressSummary } from "./types.js";
